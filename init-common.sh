86fb9f0da618c4614dd066dea8f3cc92864e73ad
Phillip Alexander <glupmjoed@fafnir.dk>
2019-01-03 17:25:14 +0100

    Disable XON/XOFF flow control

--- init-common.sh
+++ init-common.sh
@@ -3,6 +3,10 @@
 export EDITOR="emacs -nw"
 export PATH=$PATH:$HOME/bin
 
+# Disable XON/XOFF flow control (Ctrl+S/Ctrl+Q) in interactive shell sessions
+# This avoids accidental freezing of output and frees Ctrl+S for GNU Readline
+[[ $- = *i* ]] && stty -ixon
+
 # Go stuff
 export PATH=$PATH:/usr/local/go/bin
 export GOPATH=$HOME
